e0ba8bcaaf7746706708e27eb9f8097f3bfec73b
Thiago <tsantosfigueira@outlook.com>
2021-03-13 18:11:13 -0400

    Added user input for name and number guess

---
+++ guessing_game.py
@@ -0,0 +1,15 @@
+#Python guessing game
+import random
+
+# generate random number
+number = random.randint(1, 10)
+
+# ask for user name
+user_name = input("Hello! Please type your name: ")
+
+# ask for user number
+user_number = input("Guess a number from 1 to 10. You have five chances to get the number right: ")
+
+
+# verify if user number is correct
+
