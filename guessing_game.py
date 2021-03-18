b47dda624441b5ee227249f8af26cf63c6925530
Nicolas Teixeira <nicolasteixeira@my.uopeople.edu>
2021-03-17 19:04:54 -0700

    Verified user's input.
    Asked user for input and verified if the user's input matched the random number generated by the program.

--- guessing_game.py
+++ guessing_game.py
@@ -7,10 +7,29 @@ number = random.randint(1, 10)
 # ask for user name
 user_name = input("Hello! Please type your name: ")
 
-# ask for user number
-user_number = input("Guess a number from 1 to 10. You have five chances to get the number right: ")
+# initialize the user tries count
+tries = 0
 
+# loop to make the program give the user 5 attempts
+while tries < 5:
 
-# verify if user number is correct
-if(user_number == number):
-    print("Its correct!")
+    # ask for user number
+    user_number = int(input("Guess a number from 1 to 10. You have five chances to get the number right: "))
+    tries += 1
+
+    # verify if user number is correct
+    if user_number == number:
+        break
+
+    elif user_number < number:
+        print("Oops, too small!\n")
+
+    elif user_number > number:
+        print("Oops, too high!\n")
+
+# Pint message to user if number os guessed or not
+if user_number == number:
+    print('Congratulations, you guessed the number! It only took you '
+          + str(tries) + ' tries!')
+else:
+    print('You did not make it! The number was ' + str(number) + '.')
